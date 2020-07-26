497626b3181649c98634d1492d787c1f21d51fc4
Xiao Rong <xr_wong@hotmail.com>
2020-07-27 02:53:40 +0800

    manage state using react hooks

--- src/components/SearchMovieComponent.js
+++ src/components/SearchMovieComponent.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function SearchMovies() {
 
+    // states
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
+
     
-    // use async/await with fetch to call the TMDB API
+    // use async/await with fetch to call the TMDb API
     const searchMovies = async (e) => { 
         e.preventDefault() // prevent page from refreshing on submit
-        console.log("submitting")
-
-        const query = 'Jurassic Park'
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=15ecfb6a33837672e969c55deb4a503f&language=en-US&query=${query}`;
 
@@ -16,7 +17,8 @@ export default function SearchMovies() {
         try {
             const res = await fetch(url); // fetch returns a promise -- use await to wait until promise settles and returns its result
             const data = await res.json(); // response.json() again returns a promise -- use await
-            console.log(data)
+
+            setMovies(data.results);
         } 
         catch (err) {
             console.error(err);
@@ -26,7 +28,9 @@ export default function SearchMovies() {
     return (
         <form className='form' onSubmit={searchMovies}>
             <label htmlFor='query' className='label'>Movie Name</label>
-            <input type='text' className='input' name='query' placeholder='i.e. Jurassic Park'></input>
+            <input type='text' className='input' name='query' placeholder='i.e. Jurassic Park'
+            value={query} 
+            onChange={(e) => setQuery(e.target.value)}></input>
             <button className='button' type='submit'>
                 Search
             </button>
