fdd8046a026b45c4a1111a6bf3077cc39d720634
Xiao Rong <xr_wong@hotmail.com>
2020-07-27 03:44:57 +0800

    display movie information

--- src/components/SearchMovieComponent.js
+++ src/components/SearchMovieComponent.js
@@ -26,14 +26,40 @@ export default function SearchMovies() {
     }
 
     return (
-        <form className='form' onSubmit={searchMovies}>
-            <label htmlFor='query' className='label'>Movie Name</label>
-            <input type='text' className='input' name='query' placeholder='i.e. Jurassic Park'
-            value={query} 
-            onChange={(e) => setQuery(e.target.value)}></input>
-            <button className='button' type='submit'>
-                Search
-            </button>
-        </form>
+        <>
+            <form className='form' onSubmit={searchMovies}>
+                <label htmlFor='query' className='label'>Movie Name</label>
+                <input type='text' className='input' name='query' placeholder='i.e. Jurassic Park'
+                value={query} 
+                onChange={(e) => setQuery(e.target.value)}></input>
+                <button className='button' type='submit'>
+                    Search
+                </button>
+            </form>
+            <div className='card-list'>
+                {movies.filter(movie => movie.poster_path) // filter to show only movies with poster image
+                        .map((movie) => (
+                            <div className='card' key={movie.id}>
+                                <img className='card--image' 
+                                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`} // endpoint to obtain image of movie poster
+                                    alt = {movie.title + ' poster'}
+                                />
+                                <div className='card--content'>
+                                    <h3 className='card--title'>{movie.title}</h3>
+                                    <p>
+                                        <small>RELEASE DATE: {movie.release_date}</small>
+                                    </p>
+                                    <p>
+                                        <small>RATING: {movie.vote_average}</small>
+                                    </p>
+                                    <p className='card--desc'>
+                                        {movie.overview}
+                                    </p>
+                                </div>
+                            </div>
+                        ))
+                }
+            </div>
+        </>
     )
 }
